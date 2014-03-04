9b6dafd5b87990f88ad3425ecbb480d5c15d4b5e
Austin Wells <acwells@smu.edu>
2014-03-04 09:49:29 -0600

    added samplePage.php

---
+++ samplePage.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <title> ... </title>
+  <meta name="description" content="website description">
+  <meta name="keywords" content="key, words">
+  <link rel="stylesheet" href="css/main.css" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Raleway:400,800' rel='stylesheet' type='text/css'>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+</script>
+  <script language="javascript"  type="text/javascript" src="js/modal.js"> </script> 
+  <script language="javascript" type="text/javascript" src="js/accountSubmit.js"> </script> 
+
+
+</head>
+<body>
+	<? include "modals.html" ?> 
+	<div class="section">
+		<h1 class="pay">  THIS IS A TEST BLOCK </h1>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, saepe, cum natus rerum eius necessitatibus tenetur quae itaque excepturi obcaecati porro ad eligendi optio fugiat suscipit soluta qui aliquid mollitia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, sunt, mollitia, nihil quibusdam temporibus vero repellendus molestias animi tempora alias ipsam eos iure quas ullam ratione eius sapiente! Rerum, omnis.
+		</p>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, dicta, molestiae, tempore porro incidunt odit ad aliquid molestias ducimus impedit praesentium nostrum ea autem blanditiis delectus sequi et nisi accusamus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, mollitia, maxime, et, harum ipsum temporibus doloremque consequuntur error ducimus sapiente adipisci at incidunt nisi voluptatibus quisquam necessitatibus esse nihil inventore.
+		</p>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero asperiores corporis natus. Libero, non vitae possimus quo animi vel minima officiis! Et praesentium sit voluptates unde? Accusantium, molestias, nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, est tempore quo veritatis rerum officia obcaecati! Corporis, facere, sunt, harum, temporibus optio minima quam dignissimos voluptates debitis magnam perspiciatis aliquid?
+		</p>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, saepe, cum natus rerum eius necessitatibus tenetur quae itaque excepturi obcaecati porro ad eligendi optio fugiat suscipit soluta qui aliquid mollitia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, sunt, mollitia, nihil quibusdam temporibus vero repellendus molestias animi tempora alias ipsam eos iure quas ullam ratione eius sapiente! Rerum, omnis.
+		</p>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, dicta, molestiae, tempore porro incidunt odit ad aliquid molestias ducimus impedit praesentium nostrum ea autem blanditiis delectus sequi et nisi accusamus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, mollitia, maxime, et, harum ipsum temporibus doloremque consequuntur error ducimus sapiente adipisci at incidunt nisi voluptatibus quisquam necessitatibus esse nihil inventore.
+		</p>
+		<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias vero asperiores corporis natus. Libero, non vitae possimus quo animi vel minima officiis! Et praesentium sit voluptates unde? Accusantium, molestias, nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, est tempore quo veritatis rerum officia obcaecati! Corporis, facere, sunt, harum, temporibus optio minima quam dignissimos voluptates debitis magnam perspiciatis aliquid?
+		</p>	
+		<input type="button" class="pay" value="showPayment"> 
+		<input type="button" class="createAccount" value="showAccount"> 
+		<input type="button" class="login" value="showLogin"> 
+
+	
+	</div>
+
+
+
+
+</body>
+</html>
