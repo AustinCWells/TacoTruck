5d430de32badc57397799117cd154cf78ff1c658
Austin Wells <acwells@smu.edu>
2014-03-04 19:34:40 -0600

    Added all the php happiness

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Taco Truck</title>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+	<link rel="stylesheet" href="css/index.css" type="text/css">
+	<link href="css/nav.css" rel="stylesheet">
+
+</head>
+<body>
+	<? include "components/nav.html" ?>
+	<? include "components/modals.html" ?> 
+
+	<div class="section">
+
+		<article id = "info">
+
+			<section id = "aboutUs">
+				<h2>About Us</h2>
+				<p></p>
+			</section>
+
+			<section id = "createAnAccount">
+				<h2>Create An Account</h2>
+			</section>
+
+			<section id = "sackInfo">
+				<div>
+					<h2>Taco Sack</h2>
+					<img src="img/taco_sack.png" style = "width = 5%">
+					<p>Your Taco Sack is in essance your cart.  As you add tacos to your order they will appear in your sack.</p>
+				</div>
+			</section>
+
+		</article>
+
+		<article id = "tacoMenu">
+		</article>
+
+	</div>
+
+</body>
+</html>
---
+++ map.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Taco Truck | Maps</title>
+	
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places" type = "text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/jquery.cookie.js"></script>  
+	<script type="text/javascript" src = "js/map.js"></script>
+	<link rel="stylesheet" href="css/main.css" type="text/css">
+	<link href="css/nav.css" rel="stylesheet">
+
+	
+</head>
+<body>
+	<? include "components/nav.html" ?>
+	<? include "components/modals.html" ?> 
+<div class="section">
+	<div id = "mapMenu">
+
+	</div>
+	<div id="map"></div>
+</div>
+</body>
+</html>
